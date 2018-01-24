cbc8660f25877a5deb1f808583d7cca53ccea1c3
Leo Singer <leo.singer@ligo.org>
2018-01-23 20:32:53 -0500

    Touch to trigger readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -119,7 +119,6 @@ html_sidebars = {
 }
 
 
-
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
